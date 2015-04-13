efe8a3ecb62088b31c1a87f9964d9a0284703f7f
Ashleigh <ashleighalmeida@gmail.com>
2015-04-13 16:02:41 -0400

    update database

--- crm.rb
+++ crm.rb
@@ -3,8 +3,6 @@ require_relative 'rolodex'
 require 'sinatra'
 require 'data_mapper'
 
-require 'data_mapper'
-
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
 class Contact
@@ -14,7 +12,7 @@ class Contact
   property :first_name, String
   property :last_name, String
   property :email, String
-  property :note, String
+  property :notes, String
 end
 
 DataMapper.finalize
@@ -26,7 +24,8 @@ get '/' do
   erb :index
 end
 
-get '/contacts' do
+get "/contacts" do
+  @contacts = Contact.all
   erb :contacts
 end
 
@@ -34,23 +33,27 @@ get '/contacts/new' do
   erb :new_contact
 end
 
-post '/contacts' do
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  @@rolodex.add_contact(new_contact)
+post "/contacts" do
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :notes => params[:notes]
+  )
   redirect to('/contacts')
 end
 
 get "/contacts/:id" do
-  @contact = @@rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :show_contact
   else
     raise Sinatra::NotFound
   end
- end
+end
 
 get "/contacts/:id/edit" do
-  @contact = @@rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :edit_contact
   else
@@ -59,7 +62,7 @@ get "/contacts/:id/edit" do
 end
 
 put "/contacts/:id" do
-  @contact = @@rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     @contact.first_name = params[:first_name]
     @contact.last_name = params[:last_name]
@@ -73,7 +76,7 @@ put "/contacts/:id" do
 end
 
 delete "/contacts/:id" do
-  @contact = @@rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     @@rolodex.remove_contact(@contact)
     redirect to("/contacts")
